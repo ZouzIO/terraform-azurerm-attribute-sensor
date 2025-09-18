10c21982340c502910e5062cf7a13e7af94b1a63
Nassizouz <121959756+Nassizouz@users.noreply.github.com>
2025-09-18 14:00:07 +0300

    Billing Acoount Support (#7)
    * Billing Account Support
    * Add `billing_account_id` var for setting the export with a billing account scope
    * Add `create_costs_export` for creating deployments that don't have cost export
    * Reporting slim deployments registrations (depends on https://github.com/ZouzIO/sensor-registration/pull/13)
    
    ---------
    
    Co-authored-by: naviteq-denys <denys@zouz.io>

--- outputs.tf
+++ outputs.tf
@@ -2,13 +2,14 @@ output "registration_details" {
   description = "Details of the registration request sent to the Attribute Sensor API."
   value = {
     "organization_id"     = var.organization_id
-    "tenant_id"           = data.azurerm_subscription.current.tenant_id
-    "subscription_id"     = data.azurerm_subscription.current.subscription_id
-    "subscription_name"   = var.subscription_name == "" ? data.azurerm_subscription.current.display_name : var.subscription_name
+    "tenant_id"           = data.azurerm_subscription.this.tenant_id
+    "subscription_id"     = data.azurerm_subscription.this.subscription_id
+    "subscription_name"   = data.azurerm_subscription.this.display_name
+    "billing_account_id"  = var.billing_account_id != "" ? var.billing_account_id : null
     "client_id"           = azurerm_user_assigned_identity.this.client_id
-    "storage_container"   = azurerm_storage_container.this.name
-    "storage_dir"         = "focus/AttributeExport"
-    "storage_account_url" = azurerm_storage_account.this.primary_blob_endpoint
+    "storage_container"   = var.create_costs_export ? azurerm_storage_container.this.0.name : null
+    "storage_dir"         = var.create_costs_export ? "focus/AttributeExport" : null
+    "storage_account_url" = var.create_costs_export ? azurerm_storage_account.this.0.primary_blob_endpoint : null
     "module_info" = {
       "version" = data.modtm_module_source.this.module_version
       "source"  = data.modtm_module_source.this.module_source
--- registration.tf
+++ registration.tf
@@ -1,31 +1,38 @@
 data "http" "attribute_registration" {
   request_headers = {
     "Content-Type"  = "application/json"
-    "Authorization" = "Bearer ${var.token}",
+    "Authorization" = "Bearer ${var.token}"
   }
 
-  url    = "https://sensor.app.attrb.io/api/v1/azure"
+  url    = "https://sensor.stage.attrb.io/api/v1/azure"
   method = "POST"
 
-  request_body = jsonencode(merge({
-    "organization_id"     = var.organization_id
-    "tenant_id"           = data.azurerm_subscription.current.tenant_id
-    "subscription_id"     = data.azurerm_subscription.current.subscription_id
-    "subscription_name"   = var.subscription_name == "" ? data.azurerm_subscription.current.display_name : var.subscription_name
-    "client_id"           = azurerm_user_assigned_identity.this.client_id
-    "storage_container"   = azurerm_storage_container.this.name
-    "storage_dir"         = "focus/AttributeExport"
-    "storage_account_url" = azurerm_storage_account.this.primary_blob_endpoint
-    "module_info" = {
-      "version" = data.modtm_module_source.this.module_version
-      "source"  = data.modtm_module_source.this.module_source
-    }
-  }))
+  request_body = jsonencode(
+    merge(
+      {
+        organization_id    = var.organization_id
+        tenant_id          = data.azurerm_subscription.this.tenant_id
+        subscription_id    = data.azurerm_subscription.this.subscription_id
+        subscription_name  = data.azurerm_subscription.this.display_name
+        client_id          = azurerm_user_assigned_identity.this.client_id
+        billing_account_id = var.billing_account_id != "" ? var.billing_account_id : null
+        module_info = {
+          version = data.modtm_module_source.this.module_version
+          source  = data.modtm_module_source.this.module_source
+        }
+      },
+      var.create_costs_export ? {
+        storage_container   = azurerm_storage_container.this[0].name
+        storage_dir         = "focus/AttributeExport"
+        storage_account_url = azurerm_storage_account.this[0].primary_blob_endpoint
+      } : {}
+    )
+  )
 
   depends_on = [
     azurerm_role_assignment.subscription,
-    azurerm_role_assignment.storage_account,
     azurerm_federated_identity_credential.this,
+    azurerm_role_assignment.storage_account,
     azapi_resource.export
   ]
 }

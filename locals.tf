eb14ab0860a822923b6b4e7e3a129080b5376047
naviteq-denys <denys@zouz.io>
2025-09-18 14:52:51 +0400

    feat(infra): backwards compatibility

--- locals.tf
+++ locals.tf
@@ -3,6 +3,6 @@ locals {
     "Monitoring Reader",
   ]
 
-  storage_account_name = var.storage_account_name == "" ? "attrb${substr(md5(data.azurerm_subscription.this.id), 0, 15)}" : var.storage_account_name
-  export_scope        = var.billing_account_id != "" ? var.billing_account_id : data.azurerm_subscription.this.id
+  storage_account_name = var.storage_account_name == "" ? "attrb${substr(md5(data.azurerm_subscription.this.subscription_id), 0, 15)}" : var.storage_account_name
+  export_scope         = var.billing_account_id != "" ? var.billing_account_id : data.azurerm_subscription.this.id
 }
